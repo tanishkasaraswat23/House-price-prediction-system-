632f439e046a291513506d42206e9bc564d0efa1
tanishkasaraswat23 <tanishkasarswat23@gmail.com>
2026-06-12 10:52:54 +0530

    remove lesson files

--- hello.py
+++
@@ -1,7 +0,0 @@
-# This is my first Python program
-# The # symbol means this line is a comment - Python ignores it
-# Comments are notes for humans reading the code
-
-print("Hello! I am learning Python.")
-print("I am going to build a House Price Prediction System.")
-print("This is the beginning of my journey!")
